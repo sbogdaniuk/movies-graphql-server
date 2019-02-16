19a0069b8805d9bc5d03221159975f8105c40e37
sbogdaniuk <sergei.bogdaniuk@gmail.com>
2019-02-16 21:42:47 +0200

    update REST API for production

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+const dev = process.env.NODE_ENV !== 'production'
+
 export const SECRET = 'aslkdjlkaj10830912039jlkoaiuwerasdjflkasd'
 export const SECRET2 = 'ajsdklfjaskljgklasjoiquw01982310nlksas;sdlkfj'
-export const API = 'http://localhost:4000/api'
+export const API = dev ? 'http://localhost:4000/api' : 'https://movies-graphql-server.glitch.me/api/'
